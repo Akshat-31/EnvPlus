2a98039e768d2917fc516ae268ef7496d57b370a
Mohammad Azam <mohdazam7727@gmail.com>
2025-11-26 13:40:03 -0500

    Added new features to project

--- newsfeed/models.py
+++ newsfeed/models.py
@@ -107,11 +107,4 @@ class ReadHistory(TimestampedModel):
     def __str__(self):
         return f"{self.user.username} read {self.article.title}"
 
-class LoginHistory(TimestampedModel):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='login_history')
-    logged_in_at = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
-        return f"{self.user.username} logged in at {self.logged_in_at}"
-
-    
